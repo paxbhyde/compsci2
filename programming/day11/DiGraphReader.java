de69d01806fdb1ac14bf5df830dd15f317ba51c7
paxbhyde <34036930+paxbhyde@users.noreply.github.com>
2017-12-11 10:33:01 -0700

    day11 scaffold code added

---
+++ programming/day11/DiGraphReader.java
@@ -0,0 +1,48 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+/**
+ * Class that is capable of reading in a graph file from disk.
+ * Graph files are line based. Node names have type String and edge weights have 
+ * type Double. Fields on the line are separated by ':' and there is no extra white space.
+ */
+public class DiGraphReader implements IGraphReader {
+    // Fields needed for the Graph Reader should be added here
+    
+    /**
+     * Creates a new graph reader instance
+     */
+    public DiGraphReader() {
+        // Configure the graph reader here
+    }
+    
+    /**
+     * Reads in a file and instantiates the graph
+     * @param filename the file to read
+     * @return the instantiated graph
+     */
+    public IGraph<String,Double> read(String filename) throws FileNotFoundException, IOException {
+        // Open the file
+
+        // Parse the lines. If a line does not have exactly 3 fields, ignore the line
+        // For each line, add the nodes and edge
+
+        // Return the graph instance
+        return r;
+    }
+    
+    /**
+     * Simple main method to open and process a file
+     */
+    public static void main(String[] argv) throws Exception {
+        // This code should work without modification once your reader code is working
+        IGraphReader r = new DiGraphReader();
+        IGraph<String,Double> g = r.read("graphfile.cs2");
+        IEdge<String,Double>[] edges = g.getEdgeSet();
+        for(int i=0; i<edges.length; i++) {
+            System.out.println(edges[i].getSource().getValue()+" -> "+edges[i].getDestination().getValue()+"  w: "+edges[i].getWeight());
+        }
+    }
+}
